bbfa2f0a38bbc671e29525723b3fe3c78663f916
lam thanh hiep <kclamthanhhiep@gmail.com>
2019-11-11 18:37:11 +0700

    ddd

---
+++ precache-manifest.ac7be3d70ba1d6ddb8a602043c4ccf3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffe14fe586a440e7919e1942767a21b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "509370887a833a16f5ae",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "58f2ecb1f63c479746b2",
+    "url": "/static/js/2.f011e249.chunk.js"
+  },
+  {
+    "revision": "509370887a833a16f5ae",
+    "url": "/static/js/main.878b7b3e.chunk.js"
+  },
+  {
+    "revision": "9a0d511f1703b470b986",
+    "url": "/static/js/runtime-main.dbb4db69.js"
+  }
+]);
